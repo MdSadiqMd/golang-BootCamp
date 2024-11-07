81a8ec9b7496286b3d3f7ea8711dec2dccab1c99
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-11-07 21:11:52 +0530

    Added for loop in go

--- src/2_variables/main.go
+++ src/2_variables/main.go
@@ -5,4 +5,23 @@ import "fmt"
 func main() {
 	var name string = "Sadiq"
 	fmt.Println("Hello, " + name + "!")
+
+	// constants - Immutable need to define at start
+	const (
+		port = 8080
+		host = "localhost"
+	)
+	// port++; // not allowed
+	fmt.Println("Host: ", host, " Port: ", port)
+
+	// for loop - no while loop in golang
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+	// while using for loop
+	j := 1
+	for j < 5 {
+		fmt.Println(j)
+		j++
+	}
 }
