11ff44c1efb28346022879655e3da6f07dd6b01d
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-11-09 13:01:05 +0530

    🧳 Added packages

--- src/13_packages/main.go
+++ src/13_packages/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "github.com/MdSadiqMd/golang-BootCamp/auth"
+import (
+	"fmt"
+	"github.com/MdSadiqMd/golang-BootCamp/auth"
+	"github.com/MdSadiqMd/golang-BootCamp/user"
+)
 
 func main() {
 	auth.LoginWithCredentials("sadiq", "1234")
+
+	userData := user.User{
+		Name:  "Sadiq",
+		Email: "sadiq@gmail",
+	}
+	fmt.Println(userData.Email, userData.Name)
 }
---
+++ src/13_packages/user/user.go
@@ -0,0 +1,6 @@
+package user
+
+type User struct { // struct name should be capital if we want to access it outside the package
+	Name  string
+	Email string
+}
