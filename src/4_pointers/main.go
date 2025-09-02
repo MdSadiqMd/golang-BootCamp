df8b45727b3a244e27ff3eef2d212576e2f4f83f
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-09-02 17:15:14 +0530

    Refactor pointers

--- src/4_pointers/main.go
+++ src/4_pointers/main.go
@@ -4,18 +4,15 @@ import (
 	"fmt"
 )
 
-func changeNum(num int) {
-	num = 2
-}
-
-func changeNumByValue(num *int) {
-	*num = 2
-}
-
 func main() {
-	num := 1
-	changeNum(num) // This is pass by value so value won't change
-	fmt.Println(num)
-	changeNumByValue(&num) // This is pass by reference so we can change value
-	fmt.Println(num)
+	var age int // Initialise a variable with type Int
+	age = 5     // Assign a value to it, this get stored in memory
+	fmt.Println(age)
+	var pointer *int      // Initiliase a pointer in the memory with the type you're gonna reference value too, It's int because "age" is int
+	fmt.Println(&age)     // Print the address of the variable "age"
+	pointer = &age        // Assign the address of the variable "age" to pointer
+	fmt.Println(*pointer) // Print the value to which the pointer is pointing to
+
+	*pointer = 10 // edit the value of the variable via pointer - It's called dereferencing
+	fmt.Println(pointer, *pointer, age)
 }
